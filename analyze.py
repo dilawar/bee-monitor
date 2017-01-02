3f9b9db978a0261c098f1ce13fb533874c3630b7
Dilawar Singh <dilawars@ncbs.res.in>
2017-01-02 19:02:20 +0530

    The outdata is not in order.

--- analyze.py
+++ analyze.py
@@ -15,6 +15,7 @@ __status__           = "Development"
 import sys
 import os
 import datetime 
+import numpy as np
 from collections import defaultdict
 
 __fmt__ = "%Y-%m-%dT%H:%M:%S.%f"
@@ -32,9 +33,11 @@ def get_count( infile_ ):
     states = [ HIGH ] * ncols
     count = [ 0 ] * ncols
     checkpoint = 0.0
-    outfile = open( '%s_out.txt' % infile_ , 'w' )
+    # outfile = open( '%s_out.txt' % infile_ , 'w' )
+    nlines = 0
     with open( infile_, 'r' ) as f:
         for l in f:
+            nlines += 1
             data = [ float(x) for x in filter(None, l.strip( ).split( ',' )) ]
             t = data[0]
             if t  / ( 1000 * 3600 ) >= checkpoint:
@@ -52,13 +55,25 @@ def get_count( infile_ ):
                 if states[i] == LOW and curstate == HIGH:
                     count[i] += 1
                     c = ','.join( [ str(x) for x in count ] )
-                    outfile.write( '%d,%s,%d\n' % (t, c, sum(count) ) )
+                    # outfile.write( '%d,%s,%d\n' % (t, c, sum(count) ) )
                     result.append( (t, count[:]) )
                     holes[i].append( t )
                 states[i] = curstate 
-    print( 'Results are written to %s' % outfile )
-    outfile.close( )
+    # print( 'Results are written to %s' % outfile )
+    # outfile.close( )
     return result 
+
+def renormalize( tvec, vec ):
+    global infile_ 
+    newT = np.arange( 0, max( tvec ), 1000 ) * 1e-3
+    newval = np.interp( newT, tvec, vec )
+    np.savetxt( '%s_out.csv' % infile_, np.vstack((newT, newval)).T, delimiter = ',' )
+    print( 'Renormalized data is written to %s_out.csv' % infile_ )
+    return newT, newval
+
+def diff( vec ):
+    result = np.diff( vec ) 
+    return np.convolve( result, np.ones( 1000 ) / 1000 , 'same' )
 
 def plot( result ):
     global infile_ 
@@ -66,18 +81,24 @@ def plot( result ):
     import pylab
     pylab.style.use( 'ggplot' )
     tvec, countVec = [], []
+
     for t, vals in result:
-        tvec.append( t / 3600e3 )
+        tvec.append( t )
         countVec.append( sum( vals ) )
-    pylab.subplot( 211 )
-    pylab.plot( tvec, countVec )
+
+    tvec, yvec = renormalize( tvec, countVec )
+
+    pylab.subplot( 311 )
+    # pylab.plot( tvec, yvec )
+    pylab.plot( tvec[1:] / 3600, diff(yvec) * 60 )
     pylab.legend(loc='best', framealpha=0.4)
     pylab.xlabel( 'Time (hour)' )
-    pylab.ylabel( 'Total crossing' )
-    pylab.subplot( 212 )
+    pylab.ylabel( 'Rate of crossing per minute' )
+    pylab.subplot( 312 )
     pylab.bar( range( ncols ), [ len(holes[hole]) for hole in holes ])
     pylab.xlabel( 'Hole index' )
     pylab.ylabel( 'Total crossing' )
+    pylab.subplot( 313 )
     pylab.tight_layout( )
     pylab.savefig( '%s.png' % infile_ )
     print( 'Saved to file %s.png' % infile_ )
